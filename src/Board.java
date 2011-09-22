42a4e771d6a97d24fbf5ca47c79017d0a9070a7a
Victor Hallberg <vigge19@gmail.com>
2011-09-22 13:36:11 +0200

    Update Board skeleton, use Direction enum

--- src/Board.java
+++ src/Board.java
@@ -1,20 +1,39 @@
 public class Board {
+	// Dimensions of board.
+	protected int width, height;
+
+	protected Symbol[] cells;
+
 	// Save string path while searching through the game tree.
 	// To avoid unnecessary time waste, save player's position.
 	String path;
 	int playerIndex;
 
-	Board(Board board, Move move) {
+	Board(Board board, Direction dir) {
 	}
 
-	public void doMove(Move move) {
+	public void doMove(Direction dir) {
 	}
 
-	public Move[] findPossibleMoves() {
+	public Direction[] findPossibleMoves() {
 		return null;
 	}
 
-	private boolean tryMove(Move move) {
+	// Returns true if an object (player or box) can be moved from a
+	// position towards a specified direction.
+	private boolean canMove(int pos, Direction dir) {
+		switch (dir) {
+			case UP:
+				pos -= this.width;
+				if (pos < 0) return false;
+				break;
+			case DOWN:
+				break;
+			case LEFT:
+				break;
+			case RIGHT:
+				break;
+		}
 		return false;
 	}
 }
