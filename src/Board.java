61d4d1aef9be23065f1c802cbcb0eb72182f68d3
Victor Hallberg <vigge19@gmail.com>
2011-09-22 13:56:24 +0200

    Add Board#isEmptyCell method, use it in Board#canMove to determineif a move is valid

--- src/Board.java
+++ src/Board.java
@@ -19,20 +19,34 @@ public class Board {
 		return null;
 	}
 
-	// Returns true if an object (player or box) can be moved from a
-	// position towards a specified direction.
-	private boolean canMove(int pos, Direction dir) {
+	/**
+	 * Returns true if the cell at the specified position is empty,
+	 * and a valid target for movement.
+	 */
+	public isEmptyCell(int pos) {
+		if (pos < 0 || pos >= cells.length)
+			return false;
+		Symbol c = cells[pos];
+		if (c == Symbol.WALL || c == Symbol.BOX)
+			return false;
+
+		return true;
+	}
+
+	/**
+	 * Returns true if an object (player or box) can be moved from a
+	 * position towards a specified direction.
+	 */
+	public boolean canMove(int pos, Direction dir) {
 		switch (dir) {
 			case UP:
-				pos -= this.width;
-				if (pos < 0) return false;
-				break;
+				return isEmptyCell(pos - this.width);
 			case DOWN:
-				break;
+				return isEmptyCell(pos + this.width);
 			case LEFT:
-				break;
+				return isEmptyCell(pos - 1);
 			case RIGHT:
-				break;
+				return isEmptyCell(pos + 1);
 		}
 		return false;
 	}
